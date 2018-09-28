c8d80e5c6b4c309b076105a410ba82e222c824bc
John Homer Alvero <john.alvero@VLAM03160349.local>
2018-09-28 12:50:16 +0800

    add support for centos6/7

--- create-userdata.sh
+++ create-userdata.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
 
+if [ "$#" -ne 1 ]
+then
+        echo "Usage: $0 <centos major version e.g. 6>"
+        exit
+fi
+
 part_hash=$(cat part.sh | gzip | base64)
 scheme_hash=$(cat scheme.txt | gzip | base64)
 
 
-sed "s|_part_|$part_hash|" user-data.tmpl | sed "s|_scheme_|$scheme_hash|" 
+if [ "$1" -eq "7" ]
+then
+	mytemplate=user-data-7.tmpl
+elif [ "$1" -eq "6" ]
+then
+	mytemplate=user-data-6.tmpl
+else
+	echo "invalid centos version"
+fi
+
+sed "s|_part_|$part_hash|" $mytemplate | sed "s|_scheme_|$scheme_hash|" 
